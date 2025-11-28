e46682d5a86da1d7c23dc9637c42a20e19a0e460
doãn long <doanvanlong2x2@gmail.com>
2025-11-28 23:23:02 +0700

    comiit

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,8 @@
 // src/api/client.js
 import axios from "axios";
 
-// const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
-const API_BASE="http://localhost:4000";
+const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
+// const API_BASE="http://localhost:4000";
 
 
 // biến module-level, không lưu localStorage
